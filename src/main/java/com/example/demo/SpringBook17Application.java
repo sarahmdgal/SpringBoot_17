b47eeca2b1b16f5eab08445a020136f53575d9bf
RVSW317023DT <RVSW317023DT@MC.MONTGOMERYCOLLEGE.EDU>
2017-08-23 14:10:13 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/SpringBook17Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBook17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBook17Application.class, args);
+	}
+}
